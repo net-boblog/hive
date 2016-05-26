2a74714eaefd45efec3387f32eb1df3c4cce56a7
Ninebrother <jing956899449@163.com>
2016-05-26 17:21:56 +0800

    redis使用

--- hive-service/src/test/java/com/hive/test/Redis.java
+++ hive-service/src/test/java/com/hive/test/Redis.java
@@ -1,9 +1,10 @@
 package com.hive.test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
@@ -19,7 +20,7 @@ public class Redis extends TestBase {
 	private JedisCluster jedis;
 
 //	@Test
-	public void rankAdd() {
+	public void addSet() {
 		User user1 = new User("12345", "常少鹏", 99.9);
 		User user2 = new User("12346", "王卓卓", 99.8);
 		User user3 = new User("12347", "邹雨欣", 96.8);
@@ -38,10 +39,8 @@ public class Redis extends TestBase {
 		count+=jedis.zadd("page_rank".getBytes(), user7.getScore(), SerializationUtil.serialize(user7));
 		count+=jedis.zadd("page_rank".getBytes(), user8.getScore(), SerializationUtil.serialize(user8));
 		System.out.println("COUNT="+count);
-	}
-	@Test  
-    public void gameRankShow() {  
-        Set<byte[]> set = jedis.zrevrange("page_rank".getBytes(), 0, 9);  
+		
+		Set<byte[]> set = jedis.zrevrange("page_rank".getBytes(), 0, 9);  
         Iterator<byte[]> iter = set.iterator();  
         int i = 1;  
         List<User> list = new ArrayList<User>();  
@@ -50,7 +49,55 @@ public class Redis extends TestBase {
             user.setRank(i++);  
             list.add(user);  
         }  
-        for(User user : list)   
-            System.out.println(user);  
-    }  
+        for(User user : list) {  
+            System.out.println(user);
+        }    
+	}
+//	@Test
+	public void addHash(){
+		jedis.hset("user", "id", "1000");
+		jedis.hset("user", "name", "张三");
+		jedis.hset("user", "score", "100.00");
+		jedis.hset("user", "rank", "2");
+		jedis.expire("user", 60);
+		System.out.println(jedis.hgetAll("user"));
+		jedis.hincrBy("user", "rank", 2);
+		System.out.println(jedis.hgetAll("user"));
+	}
+//	@Test
+	public void rankAdd(){	
+		jedis.hsetnx("user:12345", "name", "常少鹏");
+		jedis.hsetnx("user:12345", "score", "99.9");
+		
+		jedis.hsetnx("user:12348", "name", "郑伟山");
+		jedis.hsetnx("user:12348", "score", "99.9");
+
+
+		jedis.hsetnx("user:12346", "name", "王卓卓");
+		jedis.hsetnx("user:12346", "score", "99.8");
+		
+		jedis.hsetnx("user:12347", "name", "邹雨欣");
+		jedis.hsetnx("user:12347", "score", "96.8");
+
+		
+		Map<String, Double> map=new HashMap<String, Double>();
+		map.put("user:12345", 99.9);
+		map.put("user:12346", 99.8);
+		map.put("user:12347", 96.8);
+		map.put("user:12348", 99.9);
+		jedis.zadd("page_rank", map);
+		
+		Set<String> set=jedis.zrevrange("page_rank", 0, 9);
+		for(String str:set){
+			System.out.println(jedis.hgetAll(str));
+		}
+	}
+	@Test
+	public void addList(){
+		/*jedis.lpush("latest","1");
+		jedis.lpush("latest","2");
+		jedis.lpush("latest","4");
+		jedis.lpush("latest","3");*/
+		System.out.println(jedis.lrange("latest", 0, -1));
+	}
 }
